d47f8eafc7c35eb3e8c3511e936d852922c1f207
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2022-01-15 09:23:52 -0300

    Initial commit

---
+++ teste02.py
@@ -0,0 +1,6 @@
+Numero_um = input('Digite um numero: ')
+Numero_dois = input('Digite outro numero: ')
+numero_um = int(Numero_um)
+numero_dois = int(Numero_dois)
+soma = numero_um + numero_dois
+print('A soma dos numero é: ', soma)
