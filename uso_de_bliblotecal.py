e3028c5bedbd3d151c8e6c188af50025761712cc
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2022-01-25 08:32:44 -0300

    atualização

---
+++ uso_de_bliblotecal.py
@@ -0,0 +1,5 @@
+import math
+# importando a bliblioteca inteira 
+a =  int(input('Digite um numero:  '))
+raiz = math.sqrt(a)
+print('Esse é o valor da raiz quadrada do seu número {}'.format(math.trunc(raiz)))
