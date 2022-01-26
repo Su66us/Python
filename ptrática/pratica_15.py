c77ff31c86e414ada836e715b5a30a3bfad584ff
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2022-01-26 09:15:53 -0300

    atualização prática

---
+++ ptrática/pratica_15.py
@@ -0,0 +1,6 @@
+
+ca_opos = int(input('Digite o valor do Cateto Oposto: '))
+ca_adj = int(input('Digite o valor do Catet0o Adjasente: '))
+tang = ca_opos / ca_adj
+# "circulo"
+print('Sendo a tangente do ângulo é {:2f}, sendo o seu Cateto Oposto {}, e o seu Cateto Adjasente {}'.format(tang, ca_opos, ca_adj))
--- ptrática/prática_14.py
+++ ptrática/prática_14.py
@@ -1,5 +1,13 @@
-from math import pow, sqrt
-ca_opos = int(input('Digite o valor do Cateto Oposto: '))
-ca_adj = int(input('Digite o valor do Catet0o Adjasente: '))
+from cmath import sqrt
+import math
+ca_opos = float(input('Digite o valor do Cateto Oposto: '))
+ca_adj = float(input('Digite o valor do Catet0o Adjasente: '))
 hip = sqrt(pow(ca_opos, 2) + pow(ca_adj, 2))
 print('Sendo o Cateto Oposto {} e o Cateto Adjasente {}, o valor da sua Hipotenusa de {}'. format(ca_opos, ca_adj, hip))
+# "hipotenusa"
+
+# resolução do Guanabara
+ca_opos = float(input('Digite o valor do Cateto Oposto: '))
+ca_adj = float(input('Digite o valor do Catet0o Adjasente: '))
+hi = math.hypot(ca_opos, ca_adj)
+print('O valor da hipotenusa é {:.2f}'.format(hi))
