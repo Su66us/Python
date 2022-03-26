38036790aa674b419e61352a31d8b89d109e946f
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2022-03-26 12:42:39 -0300

    Create pratica_43.py

---
+++ prática/pratica_43.py
@@ -0,0 +1,7 @@
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
+if a > b:
+    for c in range(1,b):
+        print("meu bilau é grnade")
+else:
+    print("O B é maior")
