127e72623c0608b84bc239498bcf16fde8462c4b
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-01-20 11:52:10 -0300

    Mudando de compilador
    O Visual Studio por algum motivo com o qual desconheço parou de compilar python corretamente então apartir de agora estou mudando para o pycharm

---
+++ Alura(python)/Adivinhacao.py
@@ -0,0 +1,16 @@
+print("*********************************")
+print("Bem vindo ao jogo da adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = int(input("Digite um numero: "))
+print('Você digitou' , chute)
+if chute == 40:
+    print('acertou')
+else:
+    print('Errou')
+
+
+
+
