3de0bbb2e6d9db74d53e57a3c693008ce3252abe
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-01-20 21:01:50 -0300

    elif
    "se não se "colocando em prática

--- Alura(python)/Adivinhacao.py
+++ Alura(python)/Adivinhacao.py
@@ -4,13 +4,23 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute = int(input("Digite um numero: "))
-print('Você digitou' , chute)
-if chute == 40:
+chute_str = input("Digite um numero: ")
+
+print('Você digitou' , chute_str)
+
+chute = int(chute_str)
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+if (acertou):
     print('acertou')
 else:
-    print('Errou')
-
-
+    if(maior):
+        print('Você errou! O seu chute foi maior que o numero secreto.')
+    elif(menor):
+        print('Você errou! O seu chute foi menor que o numero secreto.')
+# if == se
+#else == se não
+#elif == se não se
 
 
