cbcca50257a9f4907cd99aa701c07a0979d3bb6d
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-01-22 10:23:03 -0300

    Update Adivinhacao.py

--- Alura(python)/Adivinhacao.py
+++ Alura(python)/Adivinhacao.py
@@ -3,22 +3,28 @@ print("Bem vindo ao jogo da adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+while(rodada < total_de_tentativas):
+    print('tentativa' , rodada, 'de', total_de_tentativas)
+    chute_str = input("Digite um numero: ")
+    print('Você digitou' , chute_str)
+    chute = int(chute_str)
+    
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+    if (acertou):
+        print('acertou')
+    else:
+        if(maior):
+            print('Você errou! O seu chute foi maior que o numero secreto.')
+        elif(menor):
+            print('Você errou! O seu chute foi menor que o numero secreto.')
+    
+    rodada = rodada + 1 
 
-chute_str = input("Digite um numero: ")
-
-print('Você digitou' , chute_str)
-
-chute = int(chute_str)
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
-if (acertou):
-    print('acertou')
-else:
-    if(maior):
-        print('Você errou! O seu chute foi maior que o numero secreto.')
-    elif(menor):
-        print('Você errou! O seu chute foi menor que o numero secreto.')
+print('Fim de jogo')
 # if == se
 #else == se não
 #elif == se não se
