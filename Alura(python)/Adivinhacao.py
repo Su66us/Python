28fe46b15f9ece0c8c927701f090bd7b36d5cc4e
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-01-23 11:58:50 -0300

    Mais um aula terminada

--- Alura(python)/Adivinhacao.py
+++ Alura(python)/Adivinhacao.py
@@ -5,23 +5,29 @@ print("*********************************")
 numero_secreto = 42
 total_de_tentativas = 3
 
-for rodada in  (1,total_de_tentativas):
+for rodada in range (1,total_de_tentativas + 1):
     #print('tentativa {} de {}' rodada, total_de_tentativas)
     print('tentativa {} de {}'.format( rodada, total_de_tentativas))
-    chute_str = input("Digite um numero: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print('Você digitou' , chute_str)
     chute = int(chute_str)
     
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
+    if(chute > 1 or chute > 100):
+        print('Você deve digitar um numero entre 1 e 100!')
+    
+        continue
     if (acertou):
         print('acertou')
+        break
     else:
         if(maior):
             print('Você errou! O seu chute foi maior que o numero secreto.')
         elif(menor):
             print('Você errou! O seu chute foi menor que o numero secreto.')
+
     
 
 print('Fim de jogo')
