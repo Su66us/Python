85de3c88e2e5dc7422b54ba460be4abb89f0a1b2
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-01-28 12:19:23 -0300

    att

--- Alura(python)/forca/forca.py
+++ Alura(python)/forca/forca.py
@@ -1,22 +1,36 @@
+import random
+
 def jogar():
     print("*********************************")
     print("Bem vindo ao jogo da Forca!      ")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
+    arq = open("pal.txt", "r")
+    palavras = []
+
+    for linha in arq:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arq.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
     erros = 0
     print(letras_acertadas)
-    #  lista [] tuplas() não pode mudar imutavel estrura de dados 
+    #  lista [] tuplas() não pode mudar imutavel estrura de dados
     # enquanto (true e true)
-    
+
     while(not enforcou and not acertou):
-        
+
         chute = input('Qual letra: ')
         chute = chute.strip().upper()
-        
+
         if(chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
@@ -24,8 +38,8 @@ def jogar():
                     letras_acertadas[index] = letra
                 index += 1
         else:
-            erros += 1 
-        
+            erros += 1
+
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
