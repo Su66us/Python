d243acef4d25958b7708baa5c30e0e2f5e24538c
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-02-02 15:16:54 -0300

    att

--- Alura(python)/oo/conta.py
+++ Alura(python)/oo/conta.py
@@ -3,17 +3,22 @@ class Conta:
     
     def __init__(self,numero, titular, saldo, limite):
         print('Construindo objeto... {}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     def extrato(self):
-        print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+        print('Saldo de {} do titular {}'.format(self.__saldo, self.__titular))
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+    def tranfere(self,valor, destino):
+        self.saca(valor)
+        destino.saca(valor)
     
+        
+# responsabilidade tem que ser coesa com a classe  
 #class receita de bolo para criar o objeto criado na de chamar a class memoria conta(referencia)= Conta(class)
 # que é chamado de objeto
 # atibuto class
