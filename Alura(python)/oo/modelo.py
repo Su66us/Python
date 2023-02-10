a14bf96b0b1e97f6e02ea875ae20173079c4104f
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-02-10 15:03:28 -0300

    Continuação da aula de ontem
    Commit era para ter sido feito ontem

--- Alura(python)/oo/modelo.py
+++ Alura(python)/oo/modelo.py
@@ -41,25 +41,48 @@ class Serie(Programa):# Class filha
       def __str__(self):
          return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
-    
-    
+class Playlist:
+   def __init__(self, nome, programa):
+      self.nome = nome
+      self._programa = programa
+
+   @property
+   def listagem(self):
+       return self._programa
+   
+   
+   @property
+   def tamanho(self):
+      return len(self._programa)      
+
+
+
+
 vingadores = Filme('Vingadores - Guerra infinita', 2018, 160 )
-vingadores.dar_like()
-vingadores.dar_like()
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+vingadores.dar_like()
+vingadores.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 atlanta.dar_like()
-atlanta.dar_like()
-atlanta.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
 
 
-filmes_e_series = [vingadores, atlanta]#lista
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]#lista
+playlist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho da playlist? {len(playlist_fim_de_semana)}')
+
+
+for programa in playlist_fim_de_semana.listagem:
 
    print(programa)
 
+print(f'tá ou não tá? {demolidor in playlist_fim_de_semana}')
 
 
 # if hasattr(programa, 'duracao'):
