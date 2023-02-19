e9158a5eca23d4eb111d48432075fd6ab24775f0
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-02-18 22:52:08 -0300

    primeira aula sobre tuplas e dicionarios

---
+++ Alura(python)/tu_e_di/main.py
@@ -0,0 +1,13 @@
+idade1 = 39
+idade2 = 30
+idade3 = 70
+idade4 = 18
+
+print(idade1)
+print(idade2)
+print(idade3)
+print(idade4)
+#lista sequencia de acesso aleatorio
+lista = [39, 30, 70, 18]
+print(len(lista))
+print(lista)
