94df8de33be5766e932d55ed73bcd87294dca4fe
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-02-20 10:43:41 -0300

    terminei mais uma aula

--- Alura(python)/tu_e_di/main.py
+++ Alura(python)/tu_e_di/main.py
@@ -309,3 +309,74 @@ for conta in contas:
   print(conta)
 
  """
+
+# Herança e polimorfismo
+
+from abc import ABCMeta, abstractclassmethod
+class Conta:# quandotem o _ é por que é deixado privado
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+
+  def deposita(self, valor):
+    self._saldo += valor
+
+  @abstractclassmethod
+  def passa_o_mes(self):
+      pass
+  
+  def __str__(self):
+    return "[>> Codigo {} Saldo {} <<]".format(self._codigo, self._saldo)
+  
+class ContaSalario: 
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+
+  def __eq__(self, outro):
+    if type(outro) != ContaSalario:
+      return False
+    else:
+      return self._codigo == outro._codigo and self._saldo == outro._saldo
+  
+  def deposita(self, valor):
+      self._saldo += valor
+  
+  def __str__(self) -> str:
+    return f"[>> Codigo {self._codigo} Saldo {self._saldo} <<]"
+class ContaMultiploSalario(ContaSalario):
+  pass
+class ContaCorrente(Conta):
+  def passa_o_mes(self):
+    self._saldo -= 2
+
+class ContaPoupança(Conta):
+    def passa_o_mes(self):
+      self._saldo *= 1.01
+      self._saldo -= 3
+  
+class ContaInvestimento(Conta): 
+  pass
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta17 = ContaPoupança(17)
+conta17.deposita(1000)
+""" 
+contas = [conta16, conta17]
+for conta in contas:
+  conta.passa_o_mes() # duck typing
+  print(conta) """
+#tupla cada posição tem um significado diferente
+# evitar usar array agora
+# evitamos usar array puro, se precisamos de trabalho numérico, é costume usar o numpy
+import array as arr
+import numpy as np
+
+""" arr.array('d', [1, 3.5])
+numeros = np.array([1, 3.5])
+numeros """
+conta1 = ContaSalario(37)
+print(conta1)
+conta2 = ContaSalario(37)
+print(conta1 == conta2)
